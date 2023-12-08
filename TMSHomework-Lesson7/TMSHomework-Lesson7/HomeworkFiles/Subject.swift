3af25c8f4d4177d03dc7e5235b5ed9674f46e8ad
Natallia Mazur <natashamazur@bk.ru>
2023-12-08 13:19:13 +0300

    New project TMSHomework-Lesson7, HomeworkFiles and descrription of the homework in ViewController have been added

---
+++ TMSHomework-Lesson7/TMSHomework-Lesson7/HomeworkFiles/Subject.swift
@@ -0,0 +1,13 @@
+//
+//  Subject.swift
+//  ClassesAndStruct
+//
+//  Created by berdy on 07.12.23.
+//
+
+import Foundation
+
+struct Subject {
+    private(set) var subjectName: String
+    private(set) var grade: String
+}

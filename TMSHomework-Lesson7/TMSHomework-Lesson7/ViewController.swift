2d761142d80044bda02cd28a35bd091b34b39d7a
Natallia Mazur <natashamazur@bk.ru>
2023-12-11 22:22:09 +0300

    Teacher's instances properties has been added

--- TMSHomework-Lesson7/TMSHomework-Lesson7/ViewController.swift
+++ TMSHomework-Lesson7/TMSHomework-Lesson7/ViewController.swift
@@ -11,19 +11,19 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        enum Subjects: String {
-            case math = "Math"
-            case biology = "Biology"
-            case history = "History"
-        }
+        
+        let math = Subject(subjectName: "Math", grade: "")
+        let history = Subject(subjectName: "History", grade: "")
+        let biology = Subject(subjectName: "Biology", grade: "")
         
         // Создание учителя
         let mathTeacher = Teacher(id: 1, firstName: "John", lastName: "Doe")
         let historyTeacher = Teacher(id: 2, firstName: "Howard", lastName: "Zinn")
         let biologyTeacher = Teacher(id: 3, firstName: "Charles", lastName: "Darvin")
-        mathTeacher.addSubjectTaught(subjectName: Subjects.math.rawValue)
-        historyTeacher.addSubjectTaught(subjectName: Subjects.history.rawValue)
-        biologyTeacher.addSubjectTaught(subjectName: Subjects.biology.rawValue)
+        
+        mathTeacher.addSubjectTaught(subjectName: math.subjectName)
+        historyTeacher.addSubjectTaught(subjectName: history.subjectName)
+        biologyTeacher.addSubjectTaught(subjectName: biology.subjectName)
         
         // Создание студентов
         let student1 = Student(id: 1, name: "Alice", lastName: "Simpson", age: 20)
@@ -38,31 +38,35 @@ class ViewController: UIViewController {
         
         print("")
         
-        student1.addSubject(subjectName: Subjects.math.rawValue)
-        student1.addSubject(subjectName: Subjects.biology.rawValue)
+        student1.addSubject(subjectName: math.subjectName)
+        student1.addSubject(subjectName: biology.subjectName)
 
-        student2.addSubject(subjectName: Subjects.history.rawValue)
-        student2.addSubject(subjectName: Subjects.math.rawValue)
+        student2.addSubject(subjectName: history.subjectName)
+        student2.addSubject(subjectName: math.subjectName)
+        
         // Учитель ставит оценки студентам по предметам
-        student1.getGrade(teacher: mathTeacher, grade:( mathTeacher.setGrade(for: student1, subjectName: Subjects.math.rawValue, grade: "A")))
-        student2.getGrade(teacher: mathTeacher, grade: (mathTeacher.setGrade(for: student2, subjectName: Subjects.math.rawValue, grade: "B+")))
+        student1.getGrade(teacher: mathTeacher, grade:( mathTeacher.setGrade(for: student1, subjectName: math.subjectName, grade: "A")))
+        student2.getGrade(teacher: mathTeacher, grade: (mathTeacher.setGrade(for: student2, subjectName: math.subjectName, grade: "B+")))
         
-        
-        
-        
-          //Вывод должен быть таким: (ID) (Имя) (Фамилия) (Предмет) (Оценка) (Кто поставил оценку)
+        //Вывод должен быть таким: (ID) (Имя) (Фамилия) (Предмет) (Оценка) (Кто поставил оценку)
         student1.studentInfo()
         student2.studentInfo()
+        
+        print("")
         
         student2.assignTeacher(teacher: historyTeacher)
         student1.assignTeacher(teacher: biologyTeacher)
         
-        student2.getGrade(teacher: historyTeacher, grade: (historyTeacher.setGrade(for: student2, subjectName: Subjects.history.rawValue, grade: "A+")))
-        student1.getGrade(teacher: biologyTeacher, grade: (biologyTeacher.setGrade(for: student2, subjectName: Subjects.biology.rawValue, grade: "C")))
+        print("")
+        
+        student2.getGrade(teacher: historyTeacher, grade: (historyTeacher.setGrade(for: student2, subjectName: history.subjectName, grade: "A+")))
+        student1.getGrade(teacher: biologyTeacher, grade: (biologyTeacher.setGrade(for: student1, subjectName: biology.subjectName, grade: "C")))
         
         student1.studentInfo()
         student2.studentInfo()
-         //У преподавателя вывод должен быть таким: (ID) (Имя) (Фамилия) (Предмет который ведет) (Студент) (Оценка которую поставил преподаватель)
+        
+        
+        //У преподавателя вывод должен быть таким: (ID) (Имя) (Фамилия) (Предмет который ведет) (Студент) (Оценка которую поставил преподаватель)
         print("")
         mathTeacher.teacherInfo()
         historyTeacher.teacherInfo()
